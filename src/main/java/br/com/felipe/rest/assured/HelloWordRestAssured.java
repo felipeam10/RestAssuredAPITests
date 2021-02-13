9836035945fd36fb538b2c6300180d789f426c8a
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-13 19:29:00 -0300

    Hello Word rest-assured

---
+++ src/main/java/br/com/felipe/rest/assured/HelloWordRestAssured.java
@@ -0,0 +1,13 @@
+package br.com.felipe.rest.assured;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class HelloWordRestAssured {
+	public static void main(String[] args) {
+		
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString());
+	}
+}
